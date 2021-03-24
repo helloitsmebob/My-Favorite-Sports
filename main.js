292fa91f639087a1771a2ca106a375be7ee1dab3
helloitsmebob <80372988+helloitsmebob@users.noreply.github.com>
2021-03-23 18:21:06 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function sportsfacts(){
+  window.alert("Sports are part of every culture past and present, but each culture has its own definition of sports. The most useful definitions are those that clarify the relationship of sports to play, games, and contests. “Play,” wrote the German theorist Carl Diem, “is purposeless activity, for its own sake, the opposite of work.” Humans work because they have to; they play because they want to. Play is autotelic—that is, it has its own goals. It is voluntary and uncoerced. Recalcitrant children compelled by their parents or teachers to compete in a game of football (soccer) are not really engaged in a sport. Neither are professional athletes if their only motivation is their paycheck. In the real world, as a practical matter, motives are frequently mixed and often quite impossible to determine. Unambiguous definition is nonetheless a prerequisite to practical determinations about what is and is not an example of play.");
+}
+function tennisfacts(){
+  window.alert("Tennis is a racket sport that can be played individually against a single opponent or between two teams of two players each. Each player uses a tennis racket that is strung with cord to strike a hollow rubber ball covered with felt over or around a net and into the opponent's court.");
+}
+function tennisfacts(){
+  window.alert("Tennis is a racket sport that can be played individually against a single opponent or between two teams of two players each. Each player uses a tennis racket that is strung with cord to strike a hollow rubber ball covered with felt over or around a net and into the opponent's court.");
+}
+function basketballfacts(){
+  window.alert("Basketball, colloquially referred to as hoops,[1] is a team sport in which two teams, most commonly of five players each, opposing one another on a rectangular court, compete with the primary objective of shooting a basketball (approximately 9.4 inches (24 cm) in diameter) through the defender's hoop (a basket 18 inches (46 cm) in diameter mounted 10 feet (3.048 m) high to a backboard at each end of the court) while preventing the opposing team from shooting through their own hoop. A field goal is worth two points, unless made from behind the three-point line, when it is worth three. After a foul, timed play stops and the player fouled or designated to shoot a technical foul is given one, two or three one-point free throws. The team with the most points at the end of the game wins, but if regulation play expires with the score tied, an additional period of play (overtime) is mandated.");
+}
+function kickballfacts(){
+  window.alert("Kickball is a game and league game, similar to baseball, invented in the United States by Dr. Emmett Dunn Angell.");
+}
